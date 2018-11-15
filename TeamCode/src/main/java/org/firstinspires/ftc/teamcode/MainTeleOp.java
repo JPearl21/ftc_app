02ee5f5d1e9a722f3d227ca9f275cf0626e970f0
JPearl21 <43004905+JPearl21@users.noreply.github.com>
2018-11-15 16:46:12 -0500

    Update TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -20,6 +20,7 @@ public class MainTeleOp extends OpMode{//cool bearl real cool
 
     // Top left, top right, bottom left, bottom right, arm motors
     private DcMotor tl, tr, bl, br, arm;
+    private CRServo intake1, intake2, intake3, intake4; //1 is tr, 2 is tl, 3 is br, 4 is bl
     // Code to run after init is hit
     public void init(){
 
@@ -33,8 +34,13 @@ public class MainTeleOp extends OpMode{//cool bearl real cool
         bl = hardwareMap.dcMotor.get("bottom_left_wheel");
         br = hardwareMap.dcMotor.get("bottom_right_wheel");
         arm = hardwareMap.dcMotor.get("arm");
+        intake1 = hardwareMap.crservo.get("intake1");
+        intake2 = hardwareMap.crservo.get("intake2");
+        intake3 = hardwareMap.crservo.get("intake3");
+        intake4 = hardwareMap.crservo.get("intake4");
 
-        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); //motor will brake when not moving
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
         // Note: motors spin clockwise by default
 
@@ -42,45 +48,69 @@ public class MainTeleOp extends OpMode{//cool bearl real cool
     }
 
     // Runs given motor at 1 when cond is true
-    private void testMotor(boolean cond, DcMotor dc, DcMotor dc2, DcMotor dc3, DcMotor dc4 ) {
-        if(cond) { dc.setPower(-1); } else { dc.setPower(0); }  //changed from 1 to -1
-        if(cond) { dc2.setPower(1); } else { dc2.setPower(0); }
-        if(cond) { dc3.setPower(1); } else { dc3.setPower(0); } //no change
-        if(cond) { dc4.setPower(-1); } else { dc4.setPower(0); } //no change
+    private void testMotor(float cond, DcMotor dc, DcMotor dc2, DcMotor dc3, DcMotor dc4 ) {
+        if(Math.abs(cond) > 0) { dc.setPower(1); } else { dc.setPower(0); }  //changed from 1 to -1
+        if(Math.abs(cond) > 0) { dc2.setPower(1); } else { dc2.setPower(0); }
+        if(Math.abs(cond) > 0) { dc3.setPower(1); } else { dc3.setPower(0); } //no change
+        if(Math.abs(cond) > 0) { dc4.setPower(1); } else { dc4.setPower(0); } //no change
     }
-    private void testMotorback(boolean cond, DcMotor dc, DcMotor dc2, DcMotor dc3, DcMotor dc4 ) {
-        if(cond) { dc.setPower(1); } else { dc.setPower(0); }   //changed from -1 to 1
-        if(cond) { dc2.setPower(-1); } else { dc2.setPower(0); } //changed from -1 to 1
-        if(cond) { dc3.setPower(-1); } else { dc3.setPower(0); }    //no change
-        if(cond) { dc4.setPower(1); } else { dc4.setPower(0); }    //no change
+    private void testMotorback(float cond, DcMotor dc, DcMotor dc2, DcMotor dc3, DcMotor dc4 ) {
+        if(Math.abs(cond) > 0) { dc.setPower(-1); } else { dc.setPower(0); }   //changed from -1 to 1
+        if(Math.abs(cond) > 0) { dc2.setPower(-1); } else { dc2.setPower(0); } //changed from -1 to 1
+        if(Math.abs(cond) > 0) { dc3.setPower(-1); } else { dc3.setPower(0); }    //no change
+        if(Math.abs(cond) > 0) { dc4.setPower(-1); } else { dc4.setPower(0); }    //no change
     }
 
-    private void testMotorright(boolean cond, DcMotor dc, DcMotor dc2, DcMotor dc3, DcMotor dc4 ) {
-        if(cond) { dc.setPower(1); } else { dc.setPower(0); }   //changed from 1 to -1
-        if(cond) { dc2.setPower(1); } else { dc2.setPower(0); }    // no change
-        if(cond) { dc3.setPower(1); } else { dc3.setPower(0); } //changed from 1 to -1
-        if(cond) { dc4.setPower(1); } else { dc4.setPower(0); } // no change
+    private void testMotorright(float cond, DcMotor dc, DcMotor dc2, DcMotor dc3, DcMotor dc4 ) {
+        if(Math.abs(cond) > 0) { dc.setPower(1); } else { dc.setPower(0); }   //changed from 1 to -1
+        if(Math.abs(cond) > 0) { dc2.setPower(1); } else { dc2.setPower(0); }    // no change
+        if(Math.abs(cond) > 0) { dc3.setPower(-1); } else { dc3.setPower(0); } //changed from 1 to -1
+        if(Math.abs(cond) > 0) { dc4.setPower(1); } else { dc4.setPower(0); } // no change
     }
-    private void testMotorleft(boolean cond, DcMotor dc, DcMotor dc2, DcMotor dc3, DcMotor dc4 ) {
-        if(cond) { dc.setPower(-1); } else { dc.setPower(0); }  //no change
-        if(cond) { dc2.setPower(-1); } else { dc2.setPower(0); }    //changed from 1 to -1
-        if(cond) { dc3.setPower(-1); } else { dc3.setPower(0); }    //no change
-        if(cond) { dc4.setPower(-1); } else { dc4.setPower(0); }    //changed from 1 to -1
+    private void testMotorleft(float cond, DcMotor dc, DcMotor dc2, DcMotor dc3, DcMotor dc4 ) {
+        if(Math.abs(cond) > 0) { dc.setPower(-1); } else { dc.setPower(0); }  //no change
+        if(Math.abs(cond) > 0) { dc2.setPower(1); } else { dc2.setPower(0); }    //changed from 1 to -1
+        if(Math.abs(cond) > 0) { dc3.setPower(-1); } else { dc3.setPower(0); }    //no change
+        if(Math.abs(cond) > 0) { dc4.setPower(1); } else { dc4.setPower(0); }    //changed from 1 to -1
     }
 
 
     // Runs repeatedly
     public void loop(){
         // Check if buttons are being pressed and run motors
-        testMotor(gamepad1.dpad_up, tl,tr,bl,br);
-        testMotorback(gamepad1.dpad_down, tl,tr,bl,br);
-        testMotorright(gamepad1.dpad_right, tl,tr,bl,br);
-        testMotorleft(gamepad1.dpad_left, tl,tr,bl,br);
+        while (gamepad1.left_stick_y > 0 && ((gamepad1.left_stick_x < gamepad1.left_stick_y))) {
+            testMotor(gamepad1.left_stick_y, tl, tr, bl, br);
+        }
+        while (gamepad1.left_stick_y <0 && (gamepad1.left_stick_y < gamepad1.left_stick_x)) {
+            testMotorback(gamepad1.left_stick_y, tl, tr, bl, br);
+        }
+        while (gamepad1.left_stick_x > 0 && (gamepad1.left_stick_x > gamepad1.left_stick_y)){
+            testMotorright(gamepad1.left_stick_x, tl, tr, bl, br);
+        }
+        while (gamepad1.left_stick_x <0 && (gamepad1.left_stick_x < gamepad1.left_stick_y)) {
+            testMotorleft(gamepad1.left_stick_x, tl, tr, bl, br);
+        }
 
         // double lift = gamepad1.left_stick_y;
         double power;
-        power    = Range.clip(-gamepad1.left_stick_y,-0.1, 0.1) ;  //change min between -1 and 0, max between 0 and 1 to make slower/faster
+        power    = Range.clip(-gamepad1.right_stick_y,-0.45, 0.45) ;  //change min between -1 and 0, max between 0 and 1 to make slower/faster
         arm.setPower(power);
+
+        if(gamepad1.a){
+            intake1.setPower(1);
+            intake2.setPower(1);
+        }
+        if(gamepad1.b) {
+            intake1.setPower(-1);
+            intake2.setPower(-1);
+        }
+        while(gamepad1.y){
+            intake1.setPower(1);
+            intake2.setPower(1);
+            intake3.setPower(0.5);
+            intake4.setPower(0.5);
+        }
+
 
 
     }
