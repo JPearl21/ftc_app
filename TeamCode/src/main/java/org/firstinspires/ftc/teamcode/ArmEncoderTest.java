b168f44a08159c5d96a39c2fe12d43ca3848364a
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2019-01-29 17:00:03 -0500

    Create ArmEncoderTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmEncoderTest.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+@TeleOp
+public class ArmEncoderTest extends OpMode{
+    private DcMotor arm;
+    @Override
+    public void init() {
+        arm = hardwareMap.dcMotor.get("arm");
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            pEncoderMotorRun(0.001,-4048,arm);
+        }
+    }
+    private void pEncoderMotorRun(double kP, double target, DcMotor driveMotor) { //nate
+        double error = Math.abs(target - driveMotor.getCurrentPosition());//obtains the robot's position
+        while (error > 1) {//allows the robot to continually operate
+            driveMotor.setPower(kP * error);
+            error = Math.abs(target - driveMotor.getCurrentPosition());
+        }
+}}
